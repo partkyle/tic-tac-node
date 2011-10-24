3eb1f92cb73663f6aabee915d49ca4fe77083292
Kyle Partidge <partkyle@gmail.com>
2011-10-23 20:31:48 -0700

    added win checking to the game server

--- game_server.js
+++ game_server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = module.exports = express.createServer();
 var io  = require('socket.io').listen(app);
 var store = require('./lib/Store').Store;
+var AI = require('./lib/tic-tac').AI;
+var _ = require('underscore')._;
 
 var servers = store.index('servers'),
     games   = store.index('games');
@@ -93,21 +95,25 @@ io.sockets.on('connection', function(socket) {
     game.board[data.move.y][data.move.x] = game.players[data.name].character;
     games.put(data.gameId, game);
 
-    // find the other player
-    var other;
-    for (var player in game.players) {
-      if (player != data.name) {
-        other = game.players[player];
+    var winner = AI.getWinner(game.board);
+    if (winner) {
+      _(game.players).each(function(player) {
+        player.socket.emit('done', {win: false});
+      });
+    } else {
+      // find the other player
+      var other;
+      for (var player in game.players) {
+        if (player != data.name) {
+          other = game.players[player];
+        }
       }
-    }
 
-    // send "turn" event to other player
-    other.socket.emit('turn', {
-      gameId: data.gameId,
-      board: game.board
-    });
-  });
-  socket.on('new game', function(data) {
-    console.log(data);
+      // send "turn" event to other player
+      other.socket.emit('turn', {
+        gameId: data.gameId,
+        board: game.board
+      });
+    }
   });
 });
